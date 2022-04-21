244000bf9eb72da11a49c54fadbdd8ae6adf2cff
Avery Kyle Arenas <kyruken@gmail.com>
2022-04-21 10:26:02 -0700

    erase comments

--- javascript.js
+++ javascript.js
@@ -1,24 +1,5 @@
-//pseudo code for whole program
-
-//for 5 rounds, call playRound
-    //playRound(getPlayerSelection(), computerPlays)
-        //getPlayerSelection prompts player to pick rock, paper, or scissors and
-        //returns that choice
-
-        //computerPlays returns rock, paper, or scissors at random
-
-        //
-
-
 
 function computerPlays () {
-    //function computerPlays randomly picks Rock, Paper, or Scissors
-
-    //pseudocode
-    //let variable choice equal a number from 1-3
-        //if number 0, return "rock"
-        //if number 1, return "paper"
-        //if number 2, return "scissors"
 
     let choice = getRandomInt(3);
 
@@ -36,7 +17,7 @@ function computerPlays () {
 }
 
 function getPlayerSelection() {
-    //function prompts player for a selection
+
     let playerSelect = prompt("Pick Rock, Paper, or Scissors");
     switch (playerSelect.toLowerCase()) {
         case "rock":
@@ -47,10 +28,8 @@ function getPlayerSelection() {
             return "scissors";
     }
 }
+
 function playRound(playerSelection, computerSelection) {
-    // function playRound simulates a round of rock paper scissors
-    // playRound takes in parameters playerSelection and computerSelection and compares
-    // returns win or loss message'
 
     playerSelection = getPlayerSelection();
     computerSelection = computerPlays();
